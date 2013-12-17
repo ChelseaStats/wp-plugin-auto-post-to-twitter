79f1e1523b067fb4974b97d2a374c4a30e714c27
ChelseaStats <ChelseaStats@users.noreply.github.com>
2013-12-17 10:59:57 +0000

    Update tcr_tweeter.php

--- src/tcr_tweeter.php
+++ src/tcr_tweeter.php
@@ -21,26 +21,21 @@ add_action('new_to_publish', 'tcr_tweeter', 10, 1);
 add_action('draft_to_publish', 'tcr_tweeter', 10, 1);
 add_action('auto-draft_to_publish', 'tcr_tweeter', 10, 1);
 add_action('pending_to_publish', 'tcr_tweeter', 10, 1);
+add_action('publish_future_post', 'tcr_future', 10, 1);
 
 /* the function */
 function tcr_tweeter($post_id)
 {
     $post= get_post($post_id);
-
     if ($post->post_type == 'post' && $post->post_status == 'publish' ) {
-
 		/* get the post that's being published */
 		$post_title = $post->post_title;
-		 
 		/* author needs a twitterid in their meta data*/
 		$author = get_the_author_meta('twitterid',$post->post_author);
-		 
 		/* get the permalink */
 		$url = get_permalink($post_id);
-		 
 		/* and shorten it */
 		$short_url = getBitlyUrl($url);
-		 
 		//check to make sure the tweet is within the 140 char limit
 		//if not, shorten and place ellipsis and leave room for link.
 		if (strlen($post_title) + strlen($short_url) > 100) {
@@ -49,13 +44,10 @@ function tcr_tweeter($post_id)
 			$post_title = substr($post_title,0,strlen($post_title) - $over_flow_count - 3);
 			$post_title .= '...';
 		}
-
 		//add in the shortened bit.ly link
-		$message =  "New: ".$post_title." - ".$short_url." by @".$author." #Chelsea #CFC";
-
+		$message =  "New: ".$post_title." - ".$short_url." by @".$author." #hashtag";
 		//call the tweet function to tweet out the message
 		goTweet($message);
-		
 		//call the mail function to mail out the message
 		tcr_email($message);
 
@@ -67,19 +59,14 @@ function tcr_tweeter($post_id)
 function tcr_future($post_id)
 {
     		$post = get_post($post_id);
-
                 /* get the post that's being published */
                 $post_title = $post->post_title;
-                 
                 /* author needs a twitterid in their meta data*/
                 $author = get_the_author_meta('twitterid',$post->post_author);
-                 
                 /* get the permalink */
                 $url = get_permalink($post_id);
-                 
                 /* and shorten it */
                 $short_url = getBitlyUrl($url);
-                 
                 //check to make sure the tweet is within the 140 char limit
                 //if not, shorten and place ellipsis and leave room for link.
                 if (strlen($post_title) + strlen($short_url) > 100) {
@@ -88,19 +75,13 @@ function tcr_future($post_id)
                         $post_title = substr($post_title,0,strlen($post_title) - $over_flow_count - 3);
                         $post_title .= '...';
                 }
-
                 //add in the shortened bit.ly link
-                $message =  "new: ".$post_title." - ".$short_url." by @".$author." #Chelsea #CFC";
-
+                $message =  "new: ".$post_title." - ".$short_url." by @".$author." #hashtag";
                 //call the tweet function to tweet out the message
                 goTweet($message);
-                
                 //call the mail function to mail out the message
                 tcr_email($message);
 }
-add_action('publish_future_post', 'tcr_future', 10, 1);
-
-
 
 /* do the tweet */
 function goTweet($message) {
@@ -114,11 +95,10 @@ function goTweet($message) {
 }
 
 
-/* do the email */
+/* do the email (a copy of what is tweeted)*/
 function tcr_email($message){
   $email = 'wordpress@xxx.co.uk';
   $title = get_bloginfo('title');
   $result=wp_mail($email,'New: '.$title, $message );
 }
- 
 ?>
